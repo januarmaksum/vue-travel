0a4acb4db9793554862501d65d1b63cdd6c44150
Januar Maksum <januarmaksum@gmail.com>
2023-01-04 03:13:58 +0700

    test html

--- app.vue
+++ app.vue
@@ -1,5 +1,8 @@
 <template>
-  <h1 class="text-3xl font-bold underline">
+  <div class="flex justify-center">
+    <img src="~/assets/images/bg.png" alt="background" />
+  </div>
+  <h1 class="text-3xl font-bold underline text-center">
     Travel App
   </h1>
 </template>
